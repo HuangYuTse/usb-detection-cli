9e4f9968eceb7ccefd15117b095c72669317e3de
huangyuce <eric19961231@gmail.com>
2020-01-03 02:09:59 +0800

    add Unit test file

--- contact.js
+++ contact.js
@@ -1,9 +1,21 @@
 const program = require('commander');
 var async = require("async");
 var usbDetect = require('usb-detection');
+var colors = require('colors');
 
-
-let logo = "           _               _      _            _            \n  _   _ ___| |__         __| | ___| |_ ___  ___| |_ ___ _ __ \n | | | / __| '_ \\ _____ / _` |/ _ \\ __/ _ \\/ __| __/ _ \\ '__|\n | |_| \\__ \\ |_) |_____| (_| |  __/ ||  __/ (__| ||  __/ |   \n  \\__,_|___/_.__/       \\__,_|\\___|\\__\\___|\\___|\\__\\___|_|\n"
+const version = '1.0.0';
+/*set console Theme*/
+colors.setTheme({
+    logo: 'rainbow',
+    boundary: 'grey',
+    info: 'green',
+    data: 'yellow',
+    help: 'cyan',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+  });
+let logo = "           _               _      _            _            \n  _   _ ___| |__         __| | ___| |_ ___  ___| |_ ___ _ __ \n | | | / __| '_ \\ _____ / _` |/ _ \\ __/ _ \\/ __| __/ _ \\ '__|\n | |_| \\__ \\ |_) |_____| (_| |  __/ ||  __/ (__| ||  __/ |   \n  \\__,_|___/_.__/       \\__,_|\\___|\\__\\___|\\___|\\__\\___|_|\n".logo
 // Detect add/insert
 /* 
            _               _      _            _            
@@ -13,19 +25,19 @@ let logo = "           _               _      _            _            \n  _   
   \__,_|___/_.__/       \__,_|\___|\__\___|\___|\__\___|_|
 */
 function task() {
-    console.log('==============================================================');
+    console.log('=============================================================='.boundary);
     usbDetect.find().then(
         function (devices) {
             for (i = 0; i < devices.length; i++) {
                 console.log('locationId:',devices[i]['locationId']);
-                console.log('vendorId:','0X'+devices[i]['vendorId'].toString(16));
-                console.log('productId:','0X'+devices[i]['productId'].toString(16));
-                console.log('deviceName:',devices[i]['deviceName']);
-                console.log('manufacturer:',devices[i]['manufacturer']);
-                console.log('serialNumber:',devices[i]['serialNumber']);
+                console.log('vendorId:',('0X'+devices[i]['vendorId'].toString(16)).data);
+                console.log('productId:',('0X'+devices[i]['productId'].toString(16)).data);
+                console.log('deviceName:',devices[i]['deviceName'].data);
+                console.log('manufacturer:',devices[i]['manufacturer'].data);
+                console.log('serialNumber:',devices[i]['serialNumber'].data);
                 console.log('deviceAddress:',devices[i]['deviceAddress']);
-                console.log('url:','https://www.the-sz.com/products/usbid/index.php?v='+devices[i]['vendorId'].toString(16)+'&p='+devices[i]['productId'].toString(16));
-                console.log('--------------------------------------------------------------');
+                console.log('url:','https://www.the-sz.com/products/usbid/index.php?v='+devices[i]['vendorId'].toString(16)+'&p='+devices[i]['productId'].toString(16).data);
+                console.log('--------------------------------------------------------------'.boundary);
              }
             //console.log(devices);
             console.log("總共", devices.length, "個裝置");
@@ -34,7 +46,7 @@ function task() {
                 console.log(err);
             }).then(
                 function () {
-                    console.log('==============================================================');
+                    console.log('=============================================================='.boundary);
                 }).then(
                     function () {
                         console.log('done');
@@ -43,16 +55,25 @@ function task() {
 }
 
 program
-    .version('0.1.0')
+    .version(version)
+    .helpOption('-e, --HELP', 'read more information')
     .option('-l, --list [idtype]', 'list usb device [idtype] dec hex bin','dec')
-    .option('-P, --no-parsley', '不要香菜, Remove parsley')
+    .option('-v, --vers [idtype]', 'lookup version','0')
     .option('-t, --teatype [teatype]', '給我一杯 [紅茶]', '紅茶')
     .parse(process.argv);
 
 console.log(logo);
 if (program.list)
-    task();
-
-
+    if (program.list=='dec')
+        task();
+    else if (program.list=='hex')
+        console.log('hex');
+    else if (program.list=='bin')
+        console.log('bin');
+    else
+        console.log('else');
+if (program.vers)
+    console.log(version.info);
+ 
 
 
